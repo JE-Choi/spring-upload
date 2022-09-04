e5d7dfeb565be36719637e34683c9cd13bcbbe70
최지은 <lielieing@naver.com>
2022-09-04 21:39:53 +0900

    feat: 파일 다운로드, 이미지 리소스 조회

--- src/main/java/hello/upload/controller/ItemController.java
+++ src/main/java/hello/upload/controller/ItemController.java
@@ -6,15 +6,19 @@ import hello.upload.domain.UploadFile;
 import hello.upload.file.FileStore;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.util.UriUtils;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 @Slf4j
@@ -47,9 +51,47 @@ public class ItemController {
     }
 
     @GetMapping("/items/{id}")
-    public String items(@PathVariable Long id, Model model){
+    public String items(@PathVariable Long id, Model model) {
         Item item = itemRepository.findById(id);
         model.addAttribute("item", item);
         return "item-view";
+    }
+
+    /**
+     * 보안에 취약 - 체크로직 필요함
+     *
+     * @param filename
+     * @return
+     * @throws MalformedURLException
+     */
+    @ResponseBody
+    @GetMapping("/images/{filename}")
+    public Resource downloadImage(@PathVariable String filename) throws MalformedURLException {
+        // file:/Users/../filename.png
+        // 해당 경로의 파일에 접근해서 스트림으로 반환
+        return new UrlResource("file:" + fileStore.getFullPath(filename));
+    }
+
+
+    /**
+     * itemId에 접근할 수 있는 사용자만 다운받을 수 있도록
+     *
+     * @param itemId
+     * @return
+     */
+    @GetMapping("/attach/{itemId}")
+    public ResponseEntity<Resource> downloadAttach(@PathVariable Long itemId) throws MalformedURLException {
+        Item item = itemRepository.findById(itemId);
+        String storeFileName = item.getAttachFile().getStoreFileName();
+        String uploadFileName = item.getAttachFile().getUploadFileName();
+
+        UrlResource urlResource = new UrlResource("file:" + fileStore.getFullPath(storeFileName));
+        log.info("uploadFileName={}", uploadFileName);
+        // 한글깨짐 방지
+        String encodedUploadFileName = UriUtils.encode(uploadFileName, StandardCharsets.UTF_8);
+        String contentDisposition = "attachment; filename=\"" + encodedUploadFileName + "\"";
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, contentDisposition)
+                .body(urlResource);
     }
 }
